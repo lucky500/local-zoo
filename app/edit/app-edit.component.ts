01a4b7520afd4971a89ad39049e6aed92f56a4d3
Luciano Oliveira <luciano101@msn.com>
2017-10-27 16:52:44 -0700

    remove edit section to its own dedicated component

---
+++ app/edit/app-edit.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+
+import { Animal } from '../animal.model';
+
+@Component({
+    selector: 'app-edit',
+    moduleId: module.id,
+    templateUrl: 'app-edit.component.html'
+})
+
+export class AppEditComponent{
+
+}
